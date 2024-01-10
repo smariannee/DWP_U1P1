a5d63c574e805449504ce4a759997638161f6209
Marianne Santos <20213tn140@utez.edu.mx>
2024-01-10 08:56:03 -0600

    initial project

---
+++ Client/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue';
+</script>
+
+<template>
+  <div id="app">
+    <Navbar />
+    <b-container fluid>
+      <b-row>
+        <b-col cols="3">
+          <Sidebar />
+        </b-col>
+        <b-col cols="9">
+          <router-view />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
